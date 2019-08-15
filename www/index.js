5b009df2be8760ae12d9adfeaeb03eef4d71fce0
Evan New-Schmidt <evan@new-schmidt.com>
2019-08-15 19:33:53 -0400

    Add game of life, string version

--- www/index.js
+++ www/index.js
@@ -1,3 +1,25 @@
-import * as wasm from "wasm-game-of-life";
+import {Universe} from "wasm-game-of-life";
 
-wasm.greet("Steve");
+var is_running = true;
+
+const togglebtn = document.getElementById("toggle");
+togglebtn.addEventListener("click", () => {
+    is_running = !is_running;
+    if (is_running) {
+        requestAnimationFrame(renderLoop);
+    }
+});
+
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+const renderLoop = () => {
+    pre.textContent = universe.render();
+    universe.tick();
+
+    if (is_running) {
+        requestAnimationFrame(renderLoop);
+    }
+}
+
+requestAnimationFrame(renderLoop);
