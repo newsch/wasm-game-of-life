04bcc107e624d7452db194772c3cc6d29ec0a2ca
Evan Lloyd New-Schmidt <evan@new-schmidt.com>
2023-01-02 16:46:04 -0500

    Disable timers when debug logging not enabled

--- src/utils.rs
+++ src/utils.rs
@@ -1,31 +1,41 @@
+use log::{log_enabled, Level};
 pub use timer::Timer;
+
+const TIMER_LEVEL: Level = Level::Debug;
 
 #[cfg(not(feature = "wasm"))]
 mod timer {
+    use super::*;
     use std::time::Instant;
 
     pub struct Timer<'a> {
         name: &'a str,
-        start: Instant,
+        start: Option<Instant>,
     }
 
     impl<'a> Timer<'a> {
         pub fn new(name: &'a str) -> Timer<'a> {
-            let start = Instant::now();
+            let start = log_enabled!(TIMER_LEVEL).then(|| Instant::now());
             Timer { name, start }
         }
     }
 
     impl<'a> Drop for Timer<'a> {
         fn drop(&mut self) {
-            let time = self.start.elapsed();
-            eprintln!("Timer {} ended after {time:?}", self.name);
+            if !log_enabled!(TIMER_LEVEL) {
+                return;
+            }
+            let Some(start) = self.start else { return };
+
+            let time = start.elapsed();
+            log::debug!("Timer {} ended after {time:?}", self.name);
         }
     }
 }
 
 #[cfg(feature = "wasm")]
 mod timer {
+    use super::*;
     use web_sys::console;
 
     /// RAII wrapper around console.time()
@@ -35,14 +45,18 @@ mod timer {
 
     impl<'a> Timer<'a> {
         pub fn new(name: &'a str) -> Timer<'a> {
-            console::time_with_label(name);
+            if log_enabled!(TIMER_LEVEL) {
+                console::time_with_label(name);
+            }
             Timer { name }
         }
     }
 
     impl<'a> Drop for Timer<'a> {
         fn drop(&mut self) {
-            console::time_end_with_label(self.name);
+            if log_enabled!(TIMER_LEVEL) {
+                console::time_end_with_label(self.name);
+            }
         }
     }
 }
